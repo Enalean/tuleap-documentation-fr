3d107bcf8bbe4f6cee7fc7b7efa6b7041c9e4e0f
Thomas Gerbet <thomas.gerbet@enalean.com>
2022-01-05 12:37:04 +0100

    Use the nixpkgs pin than the Tuleap repo

--- default.nix
+++ default.nix
@@ -1,6 +1,10 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/870dbb751f4d851a3dfb554835a0c2f528386982.tar.gz") {} }:
+{ pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/1dd151f0c0c216f416e9553af08f724a2499c795.tar.gz";
+    sha256 = "14d9c178rx2q3q8x5pwa7ga9sgwm49gidlrhka8886l1mgiqba7a";
+  } ) {} 
+}:
 
-pkgs.stdenv.mkDerivation {
+pkgs.stdenvNoCC.mkDerivation {
     name = "tuleap-documentation-fr-build";
     buildInputs = [
         pkgs.gnumake
