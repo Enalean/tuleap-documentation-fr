e5d9908f450f1da7170e013a96ffea6629f46008
Thomas Gerbet <thomas.gerbet@enalean.com>
2025-10-28 12:00:56 +0100

    chore: Setup formatting/linting with treefmt, add static analysis for GHA
    This is the counterpart of:
    https://github.com/Enalean/tuleap-documentation-en/pull/1730
    https://github.com/Enalean/tuleap-documentation-en/pull/1732

--- build-support/build-tools.nix
+++ build-support/build-tools.nix
@@ -1,17 +1,21 @@
-{ pkgs ? (import ./pinned-nixpkgs.nix) { } }:
+{
+  pkgs ? (import ./pinned-nixpkgs.nix) { },
+}:
 let
   pythonWorkspace = pkgs.uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ../.; };
-  pythonSet = (pkgs.callPackage pkgs.pyproject-nix.build.packages {
-    python = pkgs.python3;
-  }).overrideScope
-    (
-      pkgs.lib.composeManyExtensions [
-        pkgs.pyproject-build-systems
-        (pythonWorkspace.mkPyprojectOverlay { sourcePreference = "wheel"; })
-      ]
-    );
+  pythonSet =
+    (pkgs.callPackage pkgs.pyproject-nix.build.packages {
+      python = pkgs.python3;
+    }).overrideScope
+      (
+        pkgs.lib.composeManyExtensions [
+          pkgs.pyproject-build-systems
+          (pythonWorkspace.mkPyprojectOverlay { sourcePreference = "wheel"; })
+        ]
+      );
   pythonVirtualenv = pythonSet.mkVirtualEnv "build-deps" pythonWorkspace.deps.all;
-in [
+in
+[
   pkgs.gnumake
   pkgs.gnugrep
   pkgs.gnused
--- build-support/dev-tools.nix
+++ build-support/dev-tools.nix
@@ -1,4 +1,16 @@
-{ pkgs }:
+{
+  pkgs,
+  treefmt-nix ? (import ./pinned-treefmt-nix.nix { }),
+}:
 [
   pkgs.uv
+  (treefmt-nix.mkWrapper pkgs {
+    programs.oxipng.enable = true;
+    programs.nixfmt = {
+      enable = true;
+      package = pkgs.nixfmt;
+    };
+    programs.actionlint.enable = true;
+    programs.zizmor.enable = true;
+  })
 ]
